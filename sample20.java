e9cae8aed99e84588b05835cfb13c250e07c45a6
Makoto Tanabe <xekid78@outlook.com>
2018-01-23 11:25:30 +0900

    メソッド

---
+++ sample20.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class sample20 {
+
+	public static void main(String[] args) {
+		sumGokei();
+
+	}
+
+	public static void sumGokei() {
+		int sum = 5 * 8;
+		System.out.println(sum);
+
+	}
+
+}
