246f6b922ffa2f3f61eb8649f1c6913e04a9f7f4
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 16:36:09 +0900

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Method {
+
+	public static void main(String[] args) {
+		sumGokei();
+
+	}
+
+	public static void sumGokei() {
+		int sum = 5 * 8;
+		System.out.println(sum);
+
+	}
+
+}
